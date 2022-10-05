029f7a260c55223f8aac151cc16e3492d379abdf
ankit <ankit.singh4596@gmail.com>
2022-10-05 09:54:36 +0530

    Fibo using recursion

---
+++ src/fiboRecursion.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class fiboRecursion {
+
+    public static int fibo(int n){
+
+
+        if (n<=1){return n;}
+
+//        f(n) = f(n-1) + f(n-2)
+        int first = fibo(n-1);
+        int last = fibo(n-2);
+
+        return first + last;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nth Fibo acc to input");
+        int n = scanner.nextInt();
+        System.out.println(fibo(n));
+    }
+}
+//0 1 1 2
