93cb025c5cfe4164e45c581a0f8a667fc55bc607
ankit <ankit.singh4596@gmail.com>
2022-10-06 21:00:39 +0530

    Updates

--- src/collectionsBasic.java
+++ src/collectionsBasic.java
@@ -1,18 +1,26 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class collectionsBasic {
+    public static void moveZeroes(int[] nums) {
+        int snowBallSize = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                snowBallSize++;
+            } else if (snowBallSize > 0) {
+                int t = nums[i];
+                nums[i] = 0;
+                System.out.println(i);
+                System.out.println(snowBallSize);
+                nums[i - snowBallSize] = t;
+            }
+        }
+    }
+
     public static void main(String[] args) {
+        int[] arr = new int[]{0,1,0,3,12};
+        moveZeroes(arr);
+        System.out.println(Arrays.toString(arr));
+    }
 
-        List<Integer> list = new ArrayList<>();
-
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        list.add(4);
-        list.add(null);
-
-        System.out.println(list);
 
     }
-}
