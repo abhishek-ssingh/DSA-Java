9e4c4b783ecc50ff7bb6d3cb478bd167f944cdf5
ankit <ankit.singh4596@gmail.com>
2022-10-06 09:21:41 +0530

    bubbleSort.java

---
+++ src/countFreqOfEle.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class countFreqOfEle {
+
+
+    public static void main(String[] args) {
+
+        int[] arr1 = new int[]{4,41,2,1,4,4,7,1,3,5,2};
+
+//        need hashmap to set each value
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int key: arr1){
+            if(map.containsKey(key)){
+                map.put(key, map.get(key) + 1);
+            } else {
+                map.put(key,1);
+            }
+        }
+
+        System.out.println(Arrays.toString(map.entrySet().toArray()));
+
+
+    }
+}
