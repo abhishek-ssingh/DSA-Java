42752db7c0ecf9ffa1c9cf96d16b2394bd8c2a68
Abhishek Singh <69783812+abhishek-ssingh@users.noreply.github.com>
2022-11-17 11:25:34 +0530

    Create RecursionBasics.java

---
+++ src/RecursionBasics.java
@@ -0,0 +1,44 @@
+package Recursion;
+
+public class RecursionBasics {
+
+    public static void printTillN(int num1,int num2){
+        if(num1>num2) return;
+
+
+        printTillN(num1,num2-1);
+        System.out.println(num2);
+    }
+    public static void printFromN(int n1,int n2){
+        if(n2<n1) return;
+        System.out.println(n2);
+        printFromN(n1,n2-1);
+
+    }
+
+    public static void sumTillN_parameterized(int i,int sum){
+        if(i<1) {
+            System.out.println(sum);
+            return;
+//            return sum;
+        }
+
+        sumTillN_parameterized(i-1,sum+i);
+    }
+
+
+    public static int sumTillN_functional(int n){
+
+        if(n==0) return 0;
+
+        return n + sumTillN_functional(n-1);
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        int sum=0;
+//        printTillN(1,n);
+//        printFromN(1,n);
+        System.out.println(sumTillN_functional(n));
+    }
+}
