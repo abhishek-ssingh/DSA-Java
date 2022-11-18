6286c696a41334155d18128c291d73aff2df82f1
Abhishek Singh <69783812+abhishek-ssingh@users.noreply.github.com>
2022-11-18 11:48:47 +0530

    Update PrintAllSubsequences.java
    All sequences through take and not take method

--- src/PrintAllSubsequences.java
+++ src/PrintAllSubsequences.java
@@ -1,34 +1,47 @@
 package Recursion;
 
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 
-public class PrintAllSubsequences {
+public class practice {
 
     public static void printAllSub(int index, ArrayList<Integer> ds, int[] arr){
 
+//        there is a concept of take and not take
+        //what we genetrally do is
+
         if(index==arr.length){
+            if(ds.size()==0){
+                System.out.println("{}");
+                return;
+            }
+
             for(int i: ds){
-                System.out.print(i + "");
+                System.out.print(i);
             }
             System.out.println();
             return;
         }
 
+//        take
+//         Y Y Y
         ds.add(arr[index]);
-        printAllSub(index+1, ds ,arr);
-
-        ds.remove(ds.size()-1);
         printAllSub(index+1,ds,arr);
 
+        ds.remove(ds.size()-1);
+
+//        not take
+//        X ............  X X x
+        printAllSub(index+1,ds,arr);
 
     }
-
 
     public static void main(String[] args) {
-        int[] arr = {3,1,2};
 
-        ArrayList<Integer> ds = new ArrayList<>();
-        printAllSub(0,ds, arr);
+        int[] arr = {3,1,2};
+        ArrayList<Integer> ds = new ArrayList<Integer>();
+        int index=0;
+
+        printAllSub(index,ds,arr);
     }
 }
+
