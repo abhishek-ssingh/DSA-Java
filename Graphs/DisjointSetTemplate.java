abc1544303b66d4d0c64f7f8c56d7456f28122f3
Abhishek Singh <69783812+abhishek-ssingh@users.noreply.github.com>
2023-02-08 15:26:20 +0530

    Update DisjointSetTemplate.java

--- Graphs/DisjointSetTemplate.java
+++ Graphs/DisjointSetTemplate.java
@@ -1,4 +1,4 @@
-import java.util.*;
+// import java.util.*;
 
 class DisjointSet {
     List<Integer> rank = new ArrayList<>();
@@ -56,23 +56,23 @@ class DisjointSet {
     }
 }
 
-class Main {
-    public static void main(String[] args) {
-        DisjointSet ds = new DisjointSet(7);
+// class Main {
+//     public static void main(String[] args) {
+//         DisjointSet ds = new DisjointSet(7);
 
-        ds.unionBySize(1, 2);
-        ds.unionBySize(2, 3);
-        ds.unionBySize(4, 5);
-        ds.unionBySize(6, 7);
-        ds.unionBySize(5, 6);
+//         ds.unionBySize(1, 2);
+//         ds.unionBySize(2, 3);
+//         ds.unionBySize(4, 5);
+//         ds.unionBySize(6, 7);
+//         ds.unionBySize(5, 6);
 
-        //if 3 and 7 are on same component or not
-        if(ds.findUPar(3) == ds.findUPar(7)) System.out.println("Same");
-        else System.out.println("Not same");
+//         //if 3 and 7 are on same component or not
+//         if(ds.findUPar(3) == ds.findUPar(7)) System.out.println("Same");
+//         else System.out.println("Not same");
 
-        ds.unionBySize(3, 7);
+//         ds.unionBySize(3, 7);
 
-        if(ds.findUPar(3) == ds.findUPar(7)) System.out.println("Same");
-        else System.out.println("Not same");
-    }
-}
+//         if(ds.findUPar(3) == ds.findUPar(7)) System.out.println("Same");
+//         else System.out.println("Not same");
+//     }
+// }
